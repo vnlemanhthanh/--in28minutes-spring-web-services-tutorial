c15c451556686ecd5aa8ebff6efdbc7642de17c8
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-15 15:08:57 +0700

    Initializing a REST API Project with Spring Boot

---
+++ restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfullwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullWebServicesApplication.class, args);
+    }
+
+}

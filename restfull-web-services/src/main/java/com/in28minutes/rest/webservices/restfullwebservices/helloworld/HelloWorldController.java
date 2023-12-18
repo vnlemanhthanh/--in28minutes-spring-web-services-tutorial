8a4c1743a1dac21de494dbb89d67b3ae9acee622
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-18 09:25:38 +0700

    Enhancing the Hello World REST API with a Path Variable

--- restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
+++ restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
@@ -5,6 +5,7 @@
 package com.in28minutes.rest.webservices.restfullwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,10 @@ public class HelloWorldController {
     @GetMapping(path = "hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
